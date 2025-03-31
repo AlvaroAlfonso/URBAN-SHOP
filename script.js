328d9a50285db7d61d810c652dee73548b9a3098
AlvaroAlfonso 3 <79542338+AlvaroAlfonso@users.noreply.github.com>
2025-03-31 15:39:04 +0000

    agrego responsive para las paginas que faltan

--- script.js
+++ script.js
@@ -5,20 +5,15 @@ const navLinks = document.querySelector('.nav-links');
 
 
 
-botonVerMas.addEventListener('mouseout', () => {
-    botonVerMas.style.transform = 'scale(1)';
-});
-
 corazones.forEach(corazon => {
     corazon.addEventListener('click', () => {
         corazon.classList.toggle('active');
     });
 });
 
-botonVerMas.addEventListener('mouseout', () => {
-    botonVerMas.style.transform = 'scale(1)';
-});
+
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
